816cb15dbf3dfbe7093a89ab3d50979c08e2dc5f
Bilal Mughal <bilal@cinqtech.com>
2024-08-07 01:05:59 +0500

    WORK:
    1. Add Networking layer.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "NetworkingLayer",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -13,14 +16,16 @@ let package = Package(
     ],
     
     dependencies: [
-        
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.9.0"),
+        .package( url: "https://github.com/tristanhimmelman/ObjectMapper.git", from: "4.2.0"),
     ],
     
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NetworkingLayer"),
+            name: "NetworkingLayer",
+            dependencies: ["Alamofire", "ObjectMapper"]),
         .testTarget(
             name: "NetworkingLayerTests",
             dependencies: ["NetworkingLayer"]),
--- Sources/NetworkingLayer/NetworkingLayer.swift
+++ Sources/NetworkingLayer/NetworkingLayer.swift
@@ -1,2 +1,46 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+import Alamofire
+import ObjectMapper
+import Foundation
+
+public class NetworkManager {
+    public static let shared = NetworkManager()
+
+    private init() {}
+
+    public func request<T: Mappable>(_ url: URLConvertible, method: HTTPMethod = .get, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, completion: @escaping (Result<T, Error>) -> Void) {
+        AF.request(url, method: method, parameters: parameters, encoding: encoding, headers: headers).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                if let mappedObject = Mapper<T>().map(JSONObject: value) {
+                    completion(.success(mappedObject))
+                } else {
+                    let error = NSError(domain: "com.yourdomain.network", code: -1, userInfo: [NSLocalizedDescriptionKey: "Mapping Error"])
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+
+    public func requestArray<T: Mappable>(_ url: URLConvertible, method: HTTPMethod = .get, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, completion: @escaping (Result<[T], Error>) -> Void) {
+        AF.request(url, method: method, parameters: parameters, encoding: encoding, headers: headers).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                if let mappedArray = Mapper<T>().mapArray(JSONObject: value) {
+                    completion(.success(mappedArray))
+                } else {
+                    let error = NSError(domain: "com.cinqtech.vault", code: -1, userInfo: [NSLocalizedDescriptionKey: "Mapping Error"])
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
+
